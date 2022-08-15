54c548fe3eb6235a331213777252a923f96facf8
GabrialGF <GabrialGF@users.noreply.github.com>
2022-08-15 09:59:37 -0500

    Create AD - Health Reporting.ps1

---
+++ Active Directory/Reporting/AD - Health Reporting.ps1
@@ -0,0 +1,16 @@
+<# Active Directory Health Reporting
+
+This will collect all relevant info for all of active directory
+
+Authors:
+Robert Wadowski
+
+Tested to work with Powershell 5.1 and 7.1
+
+Requirements:
+* Powershell 5.1 or newer or 7.1 or newer
+* Active Directory powershell module
+* Should be run on a domain controller
+
+For the latest Version or to report bugs, issue, or feature requests, please go to: https://github.com/GabrialGF/Powerhsell-Automation-Scripts.git
+#>
